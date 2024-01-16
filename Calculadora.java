0f49c95a88e0722527d581d86c460f18c646e900
ConKerazoE2906 <150805972+ConKerazoE2906@users.noreply.github.com>
2024-01-16 07:40:06 -0600

    4 basicas

--- Calculadora.java
+++ Calculadora.java
@@ -1,22 +1,74 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Calculadora {
-    private JButton button1;
-    private JButton Boton10;
-    private JButton button3;
+    private JButton Botonresta;
     private JButton button4;
-    private JButton Boton9;
-    private JButton button6;
+    private JButton button5;
     private JButton button7;
+    private JButton Botonsuma;
+    private JButton Botonmultiplicacion;
+    private JButton Botondivision;
+    private JButton button3;
+    private JButton button6;
     private JButton button8;
-    private JButton Boton11;
-    private JButton button10;
-    private JButton button11;
-    private JButton button12;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JButton button13;
-    private JButton button14;
-    private JButton Boton12;
+    private JButton button15;
     private JButton button16;
+    JPanel menuPrincipal;
+    private JLabel resultadoLbl;
+    private JTextField numero2textField;
+    private JLabel numero1Lbl;
+    private JLabel numero2Lbl;
+    private JTextField numero1textField;
+    private JLabel error;
+
+
+    public Calculadora() {
+        Botonsuma.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String r1;
+
+                resultadoLbl.setText("Resultado:" +Integer.toString( Integer.parseInt(numero1textField.getText())
+                        +Integer.parseInt(numero2textField.getText())));
+            }
+        });
+        Botonresta.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String r2;
+
+                resultadoLbl.setText("Resultado: " + Integer.toString(Integer.parseInt(numero1textField.getText())
+                        - Integer.parseInt(numero2textField.getText())));
+            }
+        });
+        Botonmultiplicacion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String r3;
+
+                resultadoLbl.setText("Resultado: " + Integer.toString(Integer.parseInt(numero1textField.getText())
+                        * Integer.parseInt(numero2textField.getText())));
+            }
+        });
+        Botondivision.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String r4;
+
+                try{
+                    resultadoLbl.setText("Resultado: " + Integer.toString(Integer.parseInt(numero1textField.getText())
+                            / Integer.parseInt(numero2textField.getText())));
+                } catch (Exception ex){
+                    Botondivision.setText("No se puede dividir para 0");
+                }
+
+            }
+        });
+    }
 }
--- Main.java
+++ Main.java
@@ -1,2 +1,14 @@
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        JFrame frame=new JFrame("Suma de dos numeros");
+        frame.setContentPane(new Calculadora().menuPrincipal);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(640,480);
+        frame.pack();
+
+        frame.setVisible(true);
+
+    }
 }
