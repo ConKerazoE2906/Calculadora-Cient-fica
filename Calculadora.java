b4fe11954883a1df438690e634640d0aed79400c
silviachaluisa <133398724+silviachaluisa@users.noreply.github.com>
2024-01-16 09:23:43 -0500

    Botones Silvia

--- Calculadora.java
+++ Calculadora.java
@@ -2,14 +2,14 @@ import javax.swing.*;
 
 public class Calculadora {
     private JButton button1;
-    private JButton button2;
+    private JButton Boton10;
     private JButton button3;
     private JButton button4;
-    private JButton button5;
+    private JButton Boton9;
     private JButton button6;
     private JButton button7;
     private JButton button8;
-    private JButton button9;
+    private JButton Boton11;
     private JButton button10;
     private JButton button11;
     private JButton button12;
@@ -17,6 +17,6 @@ public class Calculadora {
     private JTextField textField2;
     private JButton button13;
     private JButton button14;
-    private JButton button15;
+    private JButton Boton12;
     private JButton button16;
 }
